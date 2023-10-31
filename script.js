0e86a44aa57604b96abd1069f98955fa46e49286
Ganesh Khate <119408746+khateganesh@users.noreply.github.com>
2023-10-31 16:06:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+var time = 60;
+var score = 0;
+var Hitrn = 0;
+function makeBubble(){
+    var clutter = "";
+    for(var i = 0; i <= 159; i++){
+        var num = Math.floor(Math.random()*10);
+        clutter += `<div class="bubble">${num}</div>`;
+        document.querySelector("#pbtm").innerHTML = clutter;
+    }
+}
+function runTime(){
+    var timeint = setInterval(function(){
+        if(time > 0){
+            time--;
+            document.querySelector("#time").textContent = time;
+        }
+        else{
+            clearInterval(timeint);
+            document.querySelector("#pbtm").innerHTML = `<h1>Your score was ${score}</h1> <br> <a href="index.html">Play Again</a>`;
+        }
+    }, 1000)
+}
+function getNewHit(){
+    Hitrn = Math.floor(Math.random()*10);
+    document.querySelector("#Hit").textContent = Hitrn;
+}
+function incScore(){
+    score += 10;
+    document.querySelector("#score").textContent = score;
+}
+document.querySelector("#pbtm").addEventListener("click", function(dets){
+    var clickedNum = Number(dets.target.textContent);
+    if(clickedNum === Hitrn){
+        incScore();
+        makeBubble();
+        getNewHit();
+    }
+});
+runTime(); 
+makeBubble();
+getNewHit();
